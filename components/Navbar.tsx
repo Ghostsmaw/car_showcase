2574b5ea67761d5b2ce6cd017d0986ac7c7b2775
ghostsmaw <ghostsmaw@gmail.com>
2023-06-18 03:04:26 +0100

    Car stand project built with Next and used rapid api

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
+
+const NavBar = () => (
+  <header className='w-full  absolute z-10'>
+    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+      <Link href='/' className='flex justify-center items-center'>
+        <h3 style={{ fontWeight: "bolder", fontSize: "20px" }}>Carz<span style={{ color: "green" }}>Stand</span></h3>
+      </Link>
+
+      <CustomButton
+        title='Sign in'
+        btnType='button'
+        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+      />
+    </nav>
+  </header>
+);
+
+export default NavBar;
